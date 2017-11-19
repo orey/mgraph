0a5775a00ca118f44a33684349ef9a36e8bcbdd6
micagraph.com@gmail.com <micagraph.com@gmail.com>
2017-11-19 07:51:10 +0100

    adding neo basic stuff

---
+++ micagraph/neo.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const neo4j = require('neo4j-driver').v1;
+
+//TODO: to be externalized
+const driver = neo4j.driver('bolt://localhost:7687', neo4j.auth.basic('neo4j', 'mgraph'));
+const session = driver.session();
+
+const personName = 'Alice';
+const resultPromise = session.run(
+  'CREATE (a:Person {name: $name}) RETURN a',
+  {name: personName}
+);
+
+resultPromise.then(result => {
+  session.close();
+
+  const singleRecord = result.records[0];
+  const node = singleRecord.get(0);
+
+  console.log(node.properties.name);
+
+  // on application exit:
+  driver.close();
+});
+
+
+
+/*=======================================
+ * Exports
+ ========================================*/
+module.exports = {
+//  FoafPerson : FoafPerson
+};
